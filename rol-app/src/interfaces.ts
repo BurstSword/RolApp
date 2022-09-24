90c157d8ac04884a811278aadf42316d8e89c93c
Miguel ER <miguelespigaresr@gmail.com>
2022-09-24 18:44:32 +0200

    Interfaces

---
+++ rol-app/src/interfaces.ts
@@ -0,0 +1,34 @@
+export interface Entity{
+    uid:string,
+    name:string,
+    statuses:Status[],
+    initiative:number,
+    currentHp:number,
+    maxHp:number,
+    image:string,
+    attacks:Attack[]
+}
+
+export interface Status{
+    name:string,
+    tag:string,
+    color:string,
+    rounds:number,
+    damage?:number,
+    isDebuff?:boolean
+}
+
+export interface Attack{
+    name:string,
+    dices:Dice[]
+}
+
+export interface Dice{
+    faces:number,
+    quantity:number
+}
+
+export interface Game{
+    name:string,
+    entities:Entity[],
+}
